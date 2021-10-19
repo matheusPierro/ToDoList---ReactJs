36a89498fe9d0ce368287f9d52364586518971e8
Matpierro <matpierro570@gmail.com>
2021-10-19 18:36:13 -0300

    Crie componente que exibe tarefa

---
+++ src/components/Tasks.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Task from "./Task";
+export const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task task={task}/>
+      ))}
+    </>
+  );
+};
